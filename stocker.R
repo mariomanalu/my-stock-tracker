e85d72d66b8571781d539c62a116915e152283c6
Mario Manalu <1999manalu@gmail.com>
2021-08-24 13:44:51 -0500

    Added a selectable price chart

---
+++ stocker.R
@@ -0,0 +1,59 @@
+# Installing neccessary packages
+install.packages("shinydashboard")
+install.packages("quantmod")
+install.packages("tidyquant")
+install.packages("ggplot2")
+
+# Loading the packages into the project
+library(shiny)
+library(shinydashboard)
+library(quantmod)
+library(tidyquant)
+library(ggplot2)
+
+# ui.R
+ui <- dashboardPage(
+  dashboardHeader(title = "My Stock Tracker"),
+  dashboardSidebar(),
+  dashboardBody(
+    fluidRow(
+      box (
+        title = "Price chart",
+        plotOutput ("plot", height = 250)
+        ),
+      
+      box (
+        title = "Stocks",
+        selectInput("Tickers", 
+                    "Tickers",
+                    choices = c("AAPL", "SQ"),
+                    selected = "AAPL"
+                    ),
+        textOutput("result")
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$result<- renderText({
+    paste("You chose", input$Tickers)
+  })
+  output$plot <- renderPlot({
+    stock <- tq_get(input$Tickers,
+                   from = "2021-01-01",
+                   to = "2021-08-24",
+                   get = "stock.prices")
+    stock %>%
+    ggplot(aes(x = date, y = adjusted)) +
+    geom_line() +
+    theme_classic() +
+    labs(x = 'Date',
+         y = "Adjusted Price",
+         title = "Apple price chart") +
+    scale_y_continuous(breaks = seq(0,300,10))
+  })
+}
+
+shinyApp(ui, server)
+
